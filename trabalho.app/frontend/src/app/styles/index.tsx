8477a264abde984c3be61fa2e3989b70ae8d3e17
Adriano Abner <adriano.abner@live.com>
2025-04-09 03:00:31 -0300

    fix: exportando useAutoTheme corretamente

--- trabalho.app/frontend/src/app/styles/index.tsx
+++ trabalho.app/frontend/src/app/styles/index.tsx
@@ -1,5 +1,5 @@
 export { globalStyles } from "./global";
-export { theme, lightTheme, darkTheme, autoTheme } from "./theme";
+export { theme, lightTheme, darkTheme, useAutoTheme  } from "./theme";
 export { Container, Flex, Box, Grid } from "./layout";
 export { fadeIn } from "./animations";
 export { utils } from "./utils";
